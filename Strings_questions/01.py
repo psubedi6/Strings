2c5c24c157d22f60edf379cc6948fe551d5d74af
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-10-14 21:02:08 -0400

    01.py-Create a string variable name with your full name. Print: -The first character -The last character -The length of the string

---
+++ Strings_questions/01.py
@@ -0,0 +1,8 @@
+#Create a string variable name with your full name. Print:
+# The first character
+# The last character
+# The length of the string
+name="Prakash Subedi"
+print(name[0])
+print(name[-1])
+print(len(name))
