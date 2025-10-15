c53e3a5a2f7fe62e44d20100328743a9460623b5
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-10-14 21:41:43 -0400

    05.py-Take the string i love python programming  and: -Remove extra spaces from both ends -Convert it to title case -Count how many times o appears

---
+++ Strings_questions/05.py
@@ -0,0 +1,8 @@
+#Take the string " i love python programming " and:
+#Remove extra spaces from both ends
+#Convert it to title case
+#Count how many times "o" appears
+a=" i love python programming "
+print(a.strip())
+print(a.title())
+print(a.find("o"))
