6c8ccb2633e3590e292b305677c92dff604cb6d7
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-10-14 21:55:22 -0400

    07.py-Using format() , create a sentence: My name is John and I am 25 years old. by passing John and 25 as variables.

---
+++ Strings_questions/07.py
@@ -0,0 +1,6 @@
+#Using format() , create a sentence:
+#"My name is John and I am 25 years old."
+#by passing "John" and 25 as variables.
+a= input("Enter the name: ")
+b= int(input("Enter the age: "))
+print(f"My name is {a.title()} and I am {b} years old.")
