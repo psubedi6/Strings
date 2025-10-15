c0f137d0a89c6d89dffcae23840e3691d6d60156
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-10-14 21:28:02 -0400

    03.py-Given text = Python Programming , do the following: -Print the first 6 characters -Print the last 6 characters -Print every second character from the string

---
+++ Strings_questions/03.py
@@ -0,0 +1,8 @@
+#Given text = "Python Programming" , do the following:
+#Print the first 6 characters
+#Print the last 6 characters
+#Print every second character from the string
+text= "Python Programming"
+print(text[0:6])
+print(text[-7:-1])
+print(text[::2])
