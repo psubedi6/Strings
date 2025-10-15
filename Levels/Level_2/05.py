3e2828401d315e63af05ecebf03f5071fe99ac8f
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-10-15 19:31:48 -0400

    05.py-Format the string to say My name is Alice and I am 25 years old using f-strings

---
+++ Levels/Level_2/05.py
@@ -0,0 +1,4 @@
+#Format the string to say "My name is Alice and I am 25 years old" using f-strings.
+a=input("Enter your name: ")
+age=input("Enter your age: ")
+print(f"My name is {a} and I am {age} years old")
